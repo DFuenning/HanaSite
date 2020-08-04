e9595c83618e7876754403a1c337a96f4d78f6e0
DFuenning <dfuenning@gmail.com>
2020-08-04 16:32:28 -0400

    pushing up js scrollspy basics

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 $(document).ready(function () {
-  var $sections = $('.info');
-
-  $(window).scroll(function () {
-
-    var currentScroll = $(this).scrollTop();
-
-    var $currentSection 
-
-    $sections.each(function () {
-      var divPosition = $(this).offset().top;
-
-      if (divPosition - 1 < currentScroll) {
-        $currentSection = $(this);
-
-      }
-
-
-      var id = $currentSection.attr('id');
-      $('a').removeClass('active');
-      $("[href=#" + id + "]").addClass('active');
-
-    })
-
+  $(document).on("scroll", onScroll);
+  
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+      
+      $('a').each(function () {
+          $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+    
+      var target = this.hash,
+          menu = target;
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
   });
 });
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('#menu-center a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('.sidebar ul li a').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+  });
+}
